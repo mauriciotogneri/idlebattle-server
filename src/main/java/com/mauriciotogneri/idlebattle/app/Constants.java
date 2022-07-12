878be8555ccf9fe6b8f977f9024ed06593694e5c
Mauricio Togneri <mauricio.togneri@gmail.com>
2022-07-12 16:48:39 +0200

    Improvements

--- src/main/java/com/mauriciotogneri/idlebattle/app/Constants.java
+++ src/main/java/com/mauriciotogneri/idlebattle/app/Constants.java
@@ -6,9 +6,9 @@ public class Constants
 
     public static int INITIAL_MINE = 1;
     public static int INITIAL_ATTACK = 1;
-    public static int MINE_COST_MULTIPLIER = 100;
-    public static int ATTACK_COST_MULTIPLIER = 100;
-    public static int INITIAL_MONEY = 500;
+    public static int MINE_COST_MULTIPLIER = 100; // TODO: add to configuration
+    public static int ATTACK_COST_MULTIPLIER = 100; // TODO: add to configuration
+    public static int INITIAL_MONEY = 500; // TODO: add to configuration
     public static int LOST_LANE_MONEY = 500;
     public static int LINE_REWARD = 500;
 

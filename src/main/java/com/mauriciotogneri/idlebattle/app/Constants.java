18e9d2797d96eedd60c7e7e5def900f86c49f7dc
Mauricio Togneri <mauricio.togneri@gmail.com>
2022-07-12 19:43:59 +0200

    Improvements

--- src/main/java/com/mauriciotogneri/idlebattle/app/Constants.java
+++ src/main/java/com/mauriciotogneri/idlebattle/app/Constants.java
@@ -4,14 +4,14 @@ public class Constants
 {
     public static final int GAME_LOOP_STEP = 1000 / 60;
 
-    public static int MINE_COST_MULTIPLIER = 100; // TODO: add to configuration
-    public static int ATTACK_COST_MULTIPLIER = 100; // TODO: add to configuration
-    public static int INITIAL_MONEY = 500; // TODO: add to configuration
+    public static int MINE_COST_MULTIPLIER = 100; // TODO: add to configuration?
+    public static int ATTACK_COST_MULTIPLIER = 100; // TODO: add to configuration?
+    public static double BLOCK_MULTIPLIER = 50; // TODO: add to configuration?
+    public static double UNIT_BASE_DAMAGE = 0.01; // TODO: add to configuration?
+
+    public static int INITIAL_MONEY = 500;
     public static int LOST_LANE_MONEY = 500;
     public static int LINE_REWARD = 500;
-
-    public static double BLOCK_MULTIPLIER = 50;
-    public static double UNIT_BASE_DAMAGE = 0.01;
 
     public static int DIRECTION_UP = 1;
     public static int DIRECTION_DOWN = -1;

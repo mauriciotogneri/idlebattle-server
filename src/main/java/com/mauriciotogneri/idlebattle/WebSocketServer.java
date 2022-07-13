925129da180edcd741101a3a907b9601f71e863d
Mauricio Togneri <mauricio.togneri@gmail.com>
2022-07-13 10:38:14 +0200

    Improvements

---
+++ src/main/java/com/mauriciotogneri/idlebattle/WebSocketServer.java
@@ -0,0 +1,41 @@
+package com.mauriciotogneri.idlebattle;
+
+import org.glassfish.grizzly.http.server.NetworkListener;
+import org.glassfish.tyrus.server.Server;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class WebSocketServer
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            NetworkListener listener = new NetworkListener("grizzly", "0.0.0.0", 8888);
+            //listener.setSecure(true);
+            //listener.setSSLEngineConfig(new SSLEngineConfigurator(getSslContextConfigurator()).setClientMode(false).setNeedClientAuth(false));
+
+            Server server = new Server("localhost", 8888, "", new HashMap<>(), WebSocketServerEndpoint.class);
+            server.start();
+
+            Scanner scanner = new Scanner(System.in);
+            String inp = scanner.nextLine();
+            scanner.close();
+
+            if (inp.equalsIgnoreCase("t"))
+            {
+                System.out.println("[SERVER]: Server successfully terminated.....");
+                server.stop();
+            }
+            else
+            {
+                System.out.println("[SERVER]: Invalid input!!!!!");
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/mauriciotogneri/idlebattle/WebSocketServerEndpoint.java
@@ -0,0 +1,45 @@
+package com.mauriciotogneri.idlebattle;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import jakarta.websocket.CloseReason;
+import jakarta.websocket.OnClose;
+import jakarta.websocket.OnMessage;
+import jakarta.websocket.OnOpen;
+import jakarta.websocket.Session;
+import jakarta.websocket.server.ServerEndpoint;
+
+@ServerEndpoint(value = "/idlebattle")
+public class WebSocketServerEndpoint
+{
+    @OnOpen
+    public void onOpen(@NotNull Session session)
+    {
+        System.out.println("[SERVER]: Handshake successful!!!!! - Connected!!!!! - Session ID: " + session.getId());
+    }
+
+    @OnMessage
+    public String onMessage(@NotNull String message, Session session)
+    {
+        if (message.equalsIgnoreCase("terminate"))
+        {
+            try
+            {
+                session.close(new CloseReason(CloseReason.CloseCodes.NORMAL_CLOSURE, "Successfully session closed....."));
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return message;
+    }
+
+    @OnClose
+    public void onClose(@NotNull Session session, CloseReason closeReason)
+    {
+        System.out.println("[SERVER]: Session " + session.getId() + " closed, because " + closeReason);
+    }
+}

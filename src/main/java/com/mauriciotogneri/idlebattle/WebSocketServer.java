77ba142994b396e6935015b82bff04c33e0c0ad3
Mauricio Togneri <mauricio.togneri@gmail.com>
2022-07-13 11:05:27 +0200

    Improvements

--- src/main/java/com/mauriciotogneri/idlebattle/WebSocketServer.java
+++ src/main/java/com/mauriciotogneri/idlebattle/WebSocketServer.java
@@ -1,5 +1,10 @@
 package com.mauriciotogneri.idlebattle;
 
+import com.mauriciotogneri.idlebattle.ssl.SslConfig;
+
+import org.glassfish.grizzly.http.server.NetworkListener;
+import org.glassfish.grizzly.ssl.SSLEngineConfigurator;
+import org.glassfish.tyrus.client.ClientProperties;
 import org.glassfish.tyrus.server.Server;
 
 import java.util.HashMap;
@@ -14,14 +19,14 @@ public class WebSocketServer
 
         try
         {
-            /*SSLEngineConfigurator sslEngineConfigurator = new SSLEngineConfigurator(SslConfig.customContext()).setClientMode(false).setNeedClientAuth(false);
+            SSLEngineConfigurator sslEngineConfigurator = new SSLEngineConfigurator(SslConfig.customContext()).setClientMode(false).setNeedClientAuth(false);
 
             NetworkListener listener = new NetworkListener("grizzly", "localhost", 8888);
             listener.setSecure(true);
-            listener.setSSLEngineConfig(sslEngineConfigurator);*/
+            listener.setSSLEngineConfig(sslEngineConfigurator);
 
             Map<String, Object> properties = new HashMap<>();
-            //properties.put(ClientProperties.SSL_ENGINE_CONFIGURATOR, sslEngineConfigurator);
+            properties.put(ClientProperties.SSL_ENGINE_CONFIGURATOR, sslEngineConfigurator);
 
             server = new Server("localhost", 8888, "", properties, WebSocketServerEndpoint.class);
             server.start();

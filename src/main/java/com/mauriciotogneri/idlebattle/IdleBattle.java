93d4ac22f61b587b0bd25f9ff1fabcb9a3d1162e
Mauricio Togneri <mauricio.togneri@gmail.com>
2022-07-13 22:15:01 +0200

    Improvements

--- src/main/java/com/mauriciotogneri/idlebattle/IdleBattle.java
+++ src/main/java/com/mauriciotogneri/idlebattle/IdleBattle.java
@@ -2,9 +2,8 @@ package com.mauriciotogneri.idlebattle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class IdleBattle
 {
     public static void main(String[] args)
